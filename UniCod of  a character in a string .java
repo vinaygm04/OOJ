8e22806a615c1376c5aabc8a930b1ce8ed02486d
vinaygm04 <vinaygm.cs23@bmsce.ac.in>
2025-01-12 12:50:51 +0530

    Create UniCod of  a character in a string .java

---
+++ UniCod of  a character in a string .java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+class Main4{
+
+	public static void main(String args[]){
+
+		String str1 = "Hallo, World!";
+		
+        	
+        	Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the index: ");
+		int index = sc.nextInt();
+			System.out.println((int)str1.charAt(index));
+		
+			
+	}
+}
